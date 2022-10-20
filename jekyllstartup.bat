2e019adda6c0e7fe1ab497d19c0f73d5bf697f2f
onda2me <onda2me@naver.com>
2022-10-20 09:50:10 +0900

    update security

---
+++ jekyllstartup.bat
@@ -0,0 +1,15 @@
+@echo off
+rem ----------------------------
+rem :: 사용방법
+rem :: cd [서비스 디렉토리 경로]
+rem :: jekyll serve
+rem ----------------------------
+
+rem cd C:\Users\Administrator\Documents\GitHub\onda2me\
+rem jekyll serve
+
+@echo on
+rem ----------------------------
+rem Jekyll Server Start!!
+rem ----------------------------
+bundle exec jekyll serve
